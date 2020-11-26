8a0e6bf48076c626d3af1b642e2c0eec232bdcd9
fortunato <sambataro1990@gmail.com>
2020-11-26 15:01:21 +0100

    generato i 5 numeri casuali

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
+// Un alert espone 5 numeri casuali diversi.  FATTO
+// Dopo 30 secondi l’utente deve inserire, un prompt alla volta,
+ // i numeri che ha visto precedentemente.
+// Una volta inseriti i 5 numeri, il software dice quanti e quali numeri sono stati ricordati.
+
+
+
 $(document).ready(
-  function () {
-    alert("ciao")
+  function() {
+    // genero 5 numeri casuali tra 1 e 100 e li salvo in un array
+    var arrayNumber = [];
+    for (var i = 0; i < 5; i++) {
+      var generateNumber = randomNumber(1, 100);
+      arrayNumber.push(generateNumber);
+    }
+    alert(arrayNumber);
   }
-)
+  function
+);
+
+// Function-----------------------------------------<
+function randomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
